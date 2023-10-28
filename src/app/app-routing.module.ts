27c4e282068473e234979f3f4b1331deec9045a0
Theresia <theressia596@gmail.com>
2023-10-29 02:06:10 +0800

    first commit: create add, read, update, delete feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReadComponent } from './read/read.component';
+import { CreateComponent } from './create/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'create', component: CreateComponent},
+  {path: 'create/:id', component: CreateComponent},
+  {path: 'read', component: ReadComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
